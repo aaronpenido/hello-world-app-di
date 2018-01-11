9e218f0f63e7a526b99388f0838ba087d5342abb
Aaron Penido <aaron.penido@gmail.com>
2018-01-11 10:41:45 -0200

    Adição das imagens do iPhone 8

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import page1 from './images/page1.png';
+import page2 from './images/page2.png';
+import page3 from './images/page3.png';
+import page4 from './images/page4.png';
+import page5 from './images/page5.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello World</h1>
-        </header>
+      <div className="container body-content">
+        <div>
+          <img src={page1} className="img-responsive" />
+          <div className="page-separator"/>
+          <img src={page2} className="img-responsive" />
+          <div className="page-separator"/>
+          <img src={page3} className="img-responsive" />
+          <div className="page-separator"/>
+          <img src={page4} className="img-responsive" />
+          <div className="page-separator"/>
+          <img src={page5} className="img-responsive" />
+        </div>
       </div>
     );
   }
